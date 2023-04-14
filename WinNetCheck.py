b24884a356f56f6b1db8cad7cc256783fb3a350f
Adrian Wood <aawood@gmail.com>
2023-04-14 08:43:55 +0100

    Moved settings to settings.yml file

--- WinNetCheck.py
+++ WinNetCheck.py
@@ -2,17 +2,16 @@ from windows_toasts import WindowsToaster, ToastText1
 import requests
 import socket
 import time
+import yaml
 
 wintoaster = WindowsToaster('Python')
 newToast = ToastText1()
 
-active = True
-startDelay = 60
-externalIPFile = "externalIP.txt"
-serverFile = "servers.txt"
+with open('settings.yml', 'r') as file:
+    settings = yaml.safe_load(file)
 
 def toast(message):
-    if active == True:
+    if settings['active'] == True:
         newToast = ToastText1()
         newToast.SetBody(message)
         wintoaster.show_toast(newToast)
@@ -20,11 +19,11 @@ def toast(message):
         print(message)
 
 def main():
-    time.sleep(startDelay)
+    time.sleep(settings['startDelay'])
     faults = 0
     
     # Check for servers off network
-    for serverIP in open(serverFile, "r"):
+    for serverIP in open(settings['serverFile'], "r"):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(0.1)
         result = sock.connect_ex((serverIP.strip('\n'), 80))
@@ -34,7 +33,7 @@ def main():
             faults = 1
 
     # Check if External IP has changed
-    with open(externalIPFile) as f:
+    with open(settings['externalIPFile']) as f:
         expectedExtIP = f.readline().strip('\n')
 
     actualExtIP = requests.get('https://api.ipify.org')
